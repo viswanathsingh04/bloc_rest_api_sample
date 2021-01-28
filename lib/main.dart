3a93efa5611d7389aad5fc6a8ba7f10525cfe641
viswanathsingh04 <viswanathsingh04@gmail.com>
2021-01-28 19:28:05 +0530

    carousel added

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:bloc_rest_api/bloc_rest_api.dart';
 import 'package:blocrestapisample/userdata.dart';
+import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:http/http.dart' as http;
@@ -36,6 +37,8 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  int _current = 0;
+
   @override
   void initState() {
     super.initState();
@@ -56,40 +59,90 @@ class _HomePageState extends State<HomePage> {
                 return Center(child: CircularProgressIndicator());
 
               case RequestStatus.success:
-                return Center(
-                  child: Padding(
-                    padding: const EdgeInsets.all(4.0),
-                    child: ListView.separated(
-                      itemCount: state.model.data.length,
-                      separatorBuilder: (BuildContext context, int index) {
-                        return Divider(
-                          height: 2,
-                        );
-                      },
-                      itemBuilder: (BuildContext context, int index) {
-                        Datum d = state.model.data[index];
-                        return ListTile(
-                          leading: CircleAvatar(),
-                          title: Text(
-                            d.firstName,
-                            style: new TextStyle(
-                                fontWeight: FontWeight.bold, fontSize: 16.0),
-                            overflow: TextOverflow.fade,
+                return Padding(
+                  padding: const EdgeInsets.all(4.0),
+                  child: SingleChildScrollView(
+                    child: Column(
+                      children: [
+                        Container(
+                          child: CarouselSlider.builder(
+                            itemCount: state.model.data.length,
+                            options: CarouselOptions(
+                                autoPlay: true,
+                                aspectRatio: 2.0,
+                                enlargeCenterPage: true,
+                                onPageChanged: (index, reason) {
+                                  setState(() {
+                                    _current = index;
+                                  });
+                                }),
+                            itemBuilder: (context, index) {
+                              return Container(
+                                child: Center(
+                                    child: Image.network(
+                                        state.model.data[index].avatar,
+                                        fit: BoxFit.cover,
+                                        height: 200,
+                                        width: 1000)),
+                              );
+                            },
                           ),
-                          subtitle: Text(
-                            d.email,
-                            style: new TextStyle(
-                                fontWeight: FontWeight.bold, fontSize: 16.0),
-                            overflow: TextOverflow.fade,
-                          ),
-                          trailing: Text(
-                            d.id.toString(),
-                            style: new TextStyle(
-                                fontWeight: FontWeight.bold, fontSize: 16.0),
-                            overflow: TextOverflow.fade,
-                          ),
-                        );
-                      },
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: state.model.data.map((e) {
+                            int index = state.model.data.indexOf(e);
+                            return Container(
+                              width: 8.0,
+                              height: 8.0,
+                              margin: EdgeInsets.symmetric(
+                                  vertical: 10.0, horizontal: 2.0),
+                              decoration: BoxDecoration(
+                                shape: BoxShape.circle,
+                                color: _current == index
+                                    ? Color.fromRGBO(0, 0, 0, 0.9)
+                                    : Color.fromRGBO(0, 0, 0, 0.4),
+                              ),
+                            );
+                          }).toList(),
+                        ),
+                        ListView.separated(
+                          shrinkWrap: true,
+                          itemCount: state.model.data.length,
+                          separatorBuilder: (BuildContext context, int index) {
+                            return Divider(
+                              height: 2,
+                            );
+                          },
+                          itemBuilder: (BuildContext context, int index) {
+                            Datum d = state.model.data[index];
+                            return ListTile(
+                              leading: CircleAvatar(),
+                              title: Text(
+                                d.firstName,
+                                style: new TextStyle(
+                                    fontWeight: FontWeight.bold,
+                                    fontSize: 16.0),
+                                overflow: TextOverflow.fade,
+                              ),
+                              subtitle: Text(
+                                d.email,
+                                style: new TextStyle(
+                                    fontWeight: FontWeight.bold,
+                                    fontSize: 16.0),
+                                overflow: TextOverflow.fade,
+                              ),
+                              trailing: Text(
+                                d.id.toString(),
+                                style: new TextStyle(
+                                    fontWeight: FontWeight.bold,
+                                    fontSize: 16.0),
+                                overflow: TextOverflow.fade,
+                              ),
+                            );
+                          },
+                        ),
+                      ],
                     ),
                   ),
                 );
